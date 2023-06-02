2b17cf4e12825664ad3d9072abdd2625ac58e0c7
Maks Skomorokha <91227382+connars@users.noreply.github.com>
2023-06-03 01:14:13 +0300

    add colors and zones

--- src/pages/maps/Maps.js
+++ src/pages/maps/Maps.js
@@ -12,15 +12,16 @@ function Maps() {
         center: { lat: 0, lng: 0 },
         zoom: 8,
       });
-
+      const darkMapStyles = require('./darkMapStyle.json');
+      map.setOptions({ styles: darkMapStyles });
       // Создание пользовательских иконок
       const icon1 = {
-        url: 'http://plamet.ua/wp-content/uploads/2023/06/marker-3.png', // Путь к изображению для первой иконки
+        url: 'http://plamet.ua/wp-content/uploads/2023/06/marker-5.png', // Путь к изображению для первой иконки
         scaledSize: new window.google.maps.Size(50, 50), // Размер иконки
       };
 
       const icon2 = {
-        url: 'http://plamet.ua/wp-content/uploads/2023/06/marker-3.png', // Путь к изображению для второй иконки
+        url: 'http://plamet.ua/wp-content/uploads/2023/06/marker-5.png', // Путь к изображению для второй иконки
         scaledSize: new window.google.maps.Size(50, 50), // Размер иконки
       };
 
@@ -44,7 +45,45 @@ function Maps() {
       
             // Фокусировка карты на границах
             map.fitBounds(bounds);
-            
+
+
+
+            // Создание полигональной зоны
+      const zoneCoords = [
+        { lat: 48.197070, lng: 17.360877 },
+        { lat: 48.155841686555256, lng: 17.38743130593463 },
+        { lat:  48.15319274206418, lng: 17.49319914977359 },
+        { lat: 48.22131045301261, lng: 17.463679984402372 },
+      ];
+
+      const zone = new window.google.maps.Polygon({
+        paths: zoneCoords,
+        strokeColor: '#FF0000', // Цвет обводки (красный)
+        strokeOpacity: 0.8, // Прозрачность обводки (от 0 до 1)
+        strokeWeight: 2, // Толщина обводки
+        fillColor: '#FF0000', // Цвет заливки (красный)
+        fillOpacity: 0.35, // Прозрачность заливки (от 0 до 1)
+      });
+
+      // Создание информационного окна
+      const infoWindow = new window.google.maps.InfoWindow({
+        content: '<div style="color: black;">Тестовая зона 1(For Viktor)</div>',
+      });
+
+      // Добавление обработчиков событий для отображения информационного окна
+      window.google.maps.event.addListener(zone, 'mouseover', function (event) {
+        infoWindow.setPosition(event.latLng);
+        infoWindow.open(map);
+      });
+
+      window.google.maps.event.addListener(zone, 'mouseout', function () {
+        infoWindow.close();
+      });
+
+
+      // Добавление зоны на карту
+      zone.setMap(map);
+
     };
 
     // Загрузка скрипта Google Maps API
@@ -62,14 +101,14 @@ function Maps() {
 
   return (
     <>
-      <div className="map-nav">
+      {/* <div className="map-nav">
         <ul>
           <li>nav 1</li>
           <li>nav 2</li>
           <li>nav 3</li>
           <li>nav 4</li>
         </ul>
-      </div>
+      </div> */}
       <div ref={mapRef} style={{ width: '100%', height: '90vh' }}></div>
     </>
   );
